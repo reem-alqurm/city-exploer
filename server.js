7829b3413c90c4f70f3e2b3c2c71f08882bbcff6
reemalqurm <reem.alqurm@gmail.com>
2021-04-03 22:39:27 +0300

    fixed heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const superagent = require('superagent');
 const pg = require('pg');
 
-const PORT = process.env.PORT || 3000 ;
+const PORT = process.env.PORT  ;
 const DATABASE_URL = process.env.DATABASE_URL ;
 const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY ;
 const PARKS_API_KEY = process.env.PARKS_API_KEY ;
