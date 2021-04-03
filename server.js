23768267d929cbc68bca431e59bbb8594c2aa3bf
reemalqurm <reem.alqurm@gmail.com>
2021-04-03 03:00:06 +0300

    add the basic data '

--- server.js
+++ server.js
@@ -12,24 +12,79 @@ const app = express();
 app.use(cors());
 
 app.get('/location', getLocation);
+app.get('/weather', getWeather);
+app.get('/parks', getParks);
+app.get('/movies', getMovies);
+app.get('/yelp', getYelp);
 
-app.get('*',(req,res)=>{
-    console.log(`somthing hit me.`)
-    res.send('OUTCH !!')
-})
+app.get('*', notFound);
+
 app.listen(PORT , ()=> {
     console.log(`its is a live ${PORT}`);
 })
 
 function getLocation(req , res){
     res.send(
-        [
+        
             {
                 "search_query": "seattle",
                 "formatted_query": "Seattle, WA, USA",
                 "latitude": "47.606210",
                 "longitude": "-122.332071"
               }
-        ]
+        
     )
+}
+
+function getWeather(req , res){
+    res.send(
+        
+            {
+                "search_query": "seattle",
+                "formatted_query": "Seattle, WA, USA",
+                "latitude": "47.606210",
+                "longitude": "-122.332071"
+              }
+        
+    )
+}
+function getParks(req , res){
+    res.send(
+        
+            {
+                "search_query": "seattle",
+                "formatted_query": "Seattle, WA, USA",
+                "latitude": "47.606210",
+                "longitude": "-122.332071"
+              }
+        
+    )
+}
+function getMovies(req , res){
+    res.send(
+        
+            {
+                "search_query": "seattle",
+                "formatted_query": "Seattle, WA, USA",
+                "latitude": "47.606210",
+                "longitude": "-122.332071"
+              }
+        
+    )
+}
+function getYelp(req , res){
+    res.send(
+        
+            {
+                "search_query": "seattle",
+                "formatted_query": "Seattle, WA, USA",
+                "latitude": "47.606210",
+                "longitude": "-122.332071"
+              }
+        
+    )
+}
+function notFound(req, res){
+    res.status(404).send('Not Found');
+
 }
